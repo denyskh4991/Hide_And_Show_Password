2bb09bee5c3d6117f46ddf5b6a2ad179deae9d5f
denyskh4991 <denyskh4991@gmail.com>
2024-08-26 22:34:40 +0200

    Adding closing image

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let eyeIcon = document.getElementById("eyeIcon");
 let password = document.getElementById("password");
 
 eyeIcon.onclick = function () {
-    if(password.type == "password") {
+    if (password.type == "password") {
         password.type = "text";
+        eyeIcon.src = "images/eye-open.png";
     } else {
         password.type = "password";
+        eyeIcon.src = "images/eye-close.png";
     }
 }
